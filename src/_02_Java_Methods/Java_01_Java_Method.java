dadb7eca0b94c9c709ba9b1c9a0d49344f86c58d
danielOuattara <daniel.ouattara@gmx.com>
2023-01-20 18:43:10 +0100

    part2 Java methods

---
+++ src/_02_Java_Methods/Java_01_Java_Method.java
@@ -0,0 +1,54 @@
+package _02_Java_Methods;
+
+public class Java_01_Java_Method {
+
+    static void myMethod() {
+        System.out.println("My first method in Java");
+    }
+
+    public static void main(String[] args) {
+
+
+        /* Java Methods
+        ================
+
+        A method is a block of code which only runs when it is called.
+        You can pass data, known as parameters, into a method.
+        Methods are used to perform certain actions, and they are also known as functions.
+
+        Why use methods? To reuse code: define the code once, and use it many times.
+
+        Create a Method
+        ------------------
+        A method must be declared within a class. It is defined with the name of the method,
+        followed by parentheses ().
+
+        Java provides some pre-defined methods, such as System.out.println(), but you can also create
+        your own methods to perform certain actions:
+
+        Create a method inside the global Class, outside of the main function:
+
+        Example Explained:
+        - myMethod() is the name of the method
+        - static means that the method belongs to the class and not an object of the class.
+          You will learn more about objects and how to access methods through objects later in this tutorial.
+        - void means that this method does not have a return value. You will learn more about return values later
+          in this chapter
+
+        Call a Method
+        --------------
+        To call a method in Java, write the method's name followed by two parentheses () and a semicolon;
+
+        In the following example, myMethod() is used to print a text (the action), when it is called: */
+        myMethod();// Outputs "My first method in Java"
+
+        /* A method can also be called multiple times:  */
+        myMethod();
+        myMethod();
+        myMethod();
+
+        /* In the next chapter, Method Parameters, you will learn how to pass data (parameters) into a method. */
+
+    }
+
+}
---
+++ src/_02_Java_Methods/Java_04_Java_Method_Scope.java
@@ -0,0 +1,51 @@
+package _02_Java_Methods;
+
+public class Java_04_Java_Method_Scope {
+
+    public static void main(String[] args) {
+
+        /* Java Scope
+        --------------
+        In Java, variables are only accessible inside the region they are created: this is called scope.
+
+        Method Scope
+        -------------
+        Variables declared directly inside a method are available anywhere in the method following
+        the line of code in which they were declared: */
+
+            // Code here CANNOT use x
+
+            int x = 100;
+
+            // Code here can use x
+            System.out.println(x);
+
+
+       /* Block Scope
+         ------------
+
+        A block of code refers to all of the code between curly braces {}.
+        Variables declared inside blocks of code are only accessible by the code between the curly braces,
+        which follows the line in which the variable was declared: */
+
+            // Code here CANNOT use z
+
+            { // This is a block
+
+              // Code here CANNOT use z
+
+              int z = 100;
+
+              // Code here CAN use z
+              System.out.println(z);
+
+           } // The block ends here
+
+          // Code here CANNOT use z
+
+        /* A block of code may exist on its own or it can belong to an if statement,
+           while statement or for statement.
+           In the case of for statements, variables declared in the statement itself
+           are also available inside the block's scope.  */
+    }
+}
