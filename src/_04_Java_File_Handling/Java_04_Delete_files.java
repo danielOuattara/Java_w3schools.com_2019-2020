9a030e779941135bf253186cba50f1f25fa9849e
danielOuattara <daniel.ouattara@gmx.com>
2023-01-24 10:43:57 +0100

    java delete files

---
+++ src/_04_Java_File_Handling/Java_04_Delete_files.java
@@ -0,0 +1,34 @@
+package _04_Java_File_Handling;
+
+import java.io.File;  // Import the File class
+
+public class Java_04_Delete_files {
+    public static void main(String[] args) {
+
+/*      Java Delete Files
+        ==================
+
+        Delete a File
+        --------------
+        To delete a file in Java, use the delete() method: */
+
+        File fileOne = new File("filename.txt");
+        if (fileOne.delete()) {
+            System.out.println("Deleted the file: " + fileOne.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
+
+        /*    You can also delete a folder. However, it must be empty: */
+
+
+
+        File folderOne = new File("delete-folder-test");
+        if (folderOne.delete()) {
+            System.out.println("Deleted the folder: " + folderOne.getName());
+        } else {
+            System.out.println("Failed to delete the folder.");
+        }
+    }
+
+}
