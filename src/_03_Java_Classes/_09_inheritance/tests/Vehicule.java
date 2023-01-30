66192eb81ae88ab802296eb90e4c53ce9000a5a5
danielOuattara <daniel.ouattara@gmx.com>
2023-01-30 10:28:02 +0100

    inheritance

---
+++ src/_03_Java_Classes/_09_inheritance/tests/Vehicule.java
@@ -0,0 +1,10 @@
+package _03_Java_Classes._09_inheritance.tests;
+
+//final class Vehicle {
+ class Vehicle {
+    protected String brand = "Ford";        // Vehicle attribute
+
+    public void honk() {                    // Vehicle method
+        System.out.println("Tuut, tuut!");
+    }
+}
