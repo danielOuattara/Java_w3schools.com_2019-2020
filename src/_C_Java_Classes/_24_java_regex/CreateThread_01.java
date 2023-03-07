97aeb587f021c83d2bf87308391bee4567bf2a43
danielOuattara <daniel.ouattara@gmx.com>
2023-03-07 22:43:54 +0100

    java threads

---
+++ src/_C_Java_Classes/_24_java_regex/CreateThread_01.java
@@ -0,0 +1,7 @@
+package _C_Java_Classes._24_java_regex;
+
+public class CreateThread_01  extends  Thread{
+    public  void run() {
+        System.out.println("This code is running i a thread");
+    }
+}
